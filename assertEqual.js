5f258d1adb565de0a632875820d010e7312a6975
Zaynola <olaoluodushola@gmail.com>
2023-08-28 11:27:55 -0400

    implement head using a function with argument

--- assertEqual.js
+++ assertEqual.js
@@ -14,9 +14,11 @@
 
 const assertEqual = function (actual, expected) {
     if (actual === expected) {
-        console.log("👍👍👍Assertion Passed: " + [actual] + " === " + [expected])
-    } else { console.log("👎👎👎Assertion Failed: " + [actual] + " !== " + [expected]) }
-}
+        console.log(`👍👍👍Assertion Passed: ${actual}  ===  ${expected}`)
+    } else {
+        console.log(`👎👎👎Assertion Failed: ${actual} !== ${expected}`)
+    }
+};
 
 assertEqual(1, 1);
 assertEqual("Lighthouse Labs", "Bootcamp");
