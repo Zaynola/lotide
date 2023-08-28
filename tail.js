351a3b9495b0b7c20a68e6c38267a08f95ba4376
Zaynola <olaoluodushola@gmail.com>
2023-08-28 15:23:43 -0400

    add tail.js

---
+++ tail.js
@@ -0,0 +1,21 @@
+const assertEqual = function (actual, expected) {
+    if (actual === expected) {
+        console.log(`👍👍👍Assertion Passed: ${actual}  ===  ${expected}`)
+    } else {
+        console.log(`👎👎👎Assertion Failed: ${actual} !== ${expected}`)
+    }
+};
+const tail = function (array) {
+    if (array.length === 0) {
+        return undefined
+    } else {
+        return array.slice(1)
+    }
+}
+
+
+const result = tail([1, 2, 3]) // [2, 3]
+
+assertEqual(result.length, 2)
+assertEqual(result[0], 2)
+assertEqual(result[1], 3)
