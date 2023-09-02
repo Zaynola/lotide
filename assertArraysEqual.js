b84867e0b28465a8f8262d0ef76e63037c608ccf
Zaynola <olaoluodushola@gmail.com>
2023-09-02 12:48:33 -0400

    assertArray exercise

---
+++ assertArraysEqual.js
@@ -0,0 +1,22 @@
+
+const eqArrays = function (array1, array2) {
+    if (array1.length !== array2.length) {
+        return false;
+    }
+    for (let i = 0; i < array1.length; i++) {
+        if (array1[i] !== array2[i]) {
+            return false;
+        }
+    }
+    return true;
+};
+
+const assertArrayEqual = function (actual, expected) {
+    if (eqArrays(actual, expected)) {
+        console.log(`👍👍👍assert array Passed: ${actual}  ===  ${expected}`)
+    } else {
+        console.log(`👎👎👎assert array Failed: ${actual} !== ${expected}`)
+    }
+};
+
+assertArrayEqual([1, 2, 3], [1, 2, 3]);
