135adf4c69306a6e201f22eeeccd90bfd741ca25
Zaynola <olaoluodushola@gmail.com>
2023-09-06 12:58:07 -0400

    implement findKeyByValue in lotide

---
+++ findKeyByValue.js
@@ -0,0 +1,20 @@
+const assertEqual = function (actual, expected) {
+    if (actual === expected) {
+        console.log(`👍👍👍Assertion Passed: ${actual}  ===  ${expected}`)
+    } else {
+        console.log(`👎👎👎Assertion Failed: ${actual} !== ${expected}`)
+    }
+};
+
+const findKeyByValue = function(object, value){
+    return Object.keys(object).find(key => object[key] === value);
+};
+    const bestTVShowsByGenre = {
+        sci_fi: "The Expanse",
+        comedy: "Brooklyn Nine-Nine",
+        drama:  "The Wire"
+      };
+// console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"))
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
