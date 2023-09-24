ff431cda80c5a5eff35cce2b2269fba2dbad6c14
Zaynola <olaoluodushola@gmail.com>
2023-09-24 19:01:19 -0400

    modify head.js, tail.js

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -3,14 +3,3 @@ assertEqual(1, 1); //should return true
 assertEqual("Lighthouse Labs", "Bootcamp");  //should return false
 assertEqual("Lighthouse", "Lighthouse");  // should return true
 assertEqual(2, 4);   // should return false
-
-const head = require('../head')
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-
-const tail = require('../tail')
-const result = tail([1, 2, 3])
-assertEqual(result.length, 2)
-assertEqual(result[0], 2)
-assertEqual(result[1], 3)
-
---
+++ test/tailTest.js
@@ -0,0 +1,5 @@
+const tail = require('../tail')
+const result = tail([1, 2, 3])
+assertEqual(result.length, 2)
+assertEqual(result[0], 2)
+assertEqual(result[1], 3)
